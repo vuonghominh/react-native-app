fd4c1327c5e8ea9c5da5a76638cfff7db4de2f55
Leon <vuonghominh@gmail.com>
2017-05-26 16:01:00 +0700

    render match list

--- app/components/matchList.js
+++ app/components/matchList.js
@@ -9,6 +9,7 @@ import {
 } from 'native-base'
 import { observer } from 'mobx-react/native'
 import { autoSubscriber } from 'firebase-nest'
+import SwipeCards from 'react-native-swipe-cards'
 
 class MatchList extends Component {
   constructor(props) {
@@ -32,6 +33,52 @@ class MatchList extends Component {
   }
   markViewed(match) {
     this.props.stores.matches.markViewed(match[0])
+  }
+  renderCard(post, store) {
+    const postObj = post ? post[1] : null
+
+    if(postObj) {
+      let pic = {uri: postObj.url}
+      let text = postObj.text
+
+      return (
+        <View style={styles.card}>
+          {pic.uri != undefined && pic.uri != "" ? <Thumbnail source={pic} /> : null}
+          <Text style={styles.text}>
+            {text}
+          </Text>
+        </View>
+      )
+    }
+
+    return null
+  }
+  renderNoMoreCards() {
+    return (
+      <View style={styles.noMoreCards}>
+        <Text> Out of Matches </Text>
+      </View>
+    )
+  }
+  render() {
+    const { matches } = this.props.stores
+    const postList = matches.getData('matches')
+    const list = postList ? postList.entries() : null
+    const { fetching } = this.state
+
+    return (
+      <View>
+        { fetching ? <Spinner /> :
+          <SwipeCards 
+            cards={list}
+            renderCard={(card) => this.renderCard(card, matches)}
+            renderNoMoreCards={this.renderNoMoreCards}
+            handleYup={this.markViewed.bind(this)}
+            handleNope={this.markViewed.bind(this)}
+          />
+        }
+      </View>
+    )
   }
 }
 
